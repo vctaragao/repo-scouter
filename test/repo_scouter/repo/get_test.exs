1d8eff13dab0ed727594574e082ef88052ff3830
Victor Moraes de Aragão <vct.aragao@gmail.com>
2022-05-18 21:57:56 -0300

    feat: usar mock criado no teste do get

--- test/repo_scouter/repo/get_test.exs
+++ test/repo_scouter/repo/get_test.exs
@@ -1,14 +1,28 @@
 defmodule RepoScouter.Repo.GetTest do
   use RepoScouter.DataCase, async: true
 
+  import Mox
+
   alias RepoScouter.Repo.Get
   alias RepoScouter.Repository
 
+  setup :verify_on_exit!
+
   describe "get_repos/1" do
     test "When given a valid username return their repos" do
+      expect(GithubClient.ClientMock, :get_repos, fn _ ->
+        {:ok, [%{"name" => "repo_mock", "language" => "php", "url" => "url"}]}
+      end)
+
       assert {:ok, [%Repository{} | _] = repos} = Get.call(%{username: "vctaragao"})
 
-      assert 20 == length(repos)
+      assert 1 == length(repos)
+    end
+
+    test "When given an invalid username handle error" do
+      expect(GithubClient.ClientMock, :get_repos, fn _ -> {:error, "Invalid username"} end)
+
+      assert {:error, "Invalid username"} = Get.call(%{username: "vct-aragao"})
     end
   end
 end
